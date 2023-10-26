7f65de061c198fb8a97be236d273760924f6fcfe
umairarshad <umairarshad085@gmail.com>
2023-10-26 23:04:18 +0500

    Student model created Serilization file created and class for studentserilizaton views for studentdetails and list created

--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Student
+from .serializers import StudentSerilizer
+from rest_framework.renderers import JSONRenderer
 
 # Create your views here.
+
+
+def student_detail(request, pk):
+    stu = Student.objects.get(id = pk)
+    # print('student object')
+    # print(stu)
+    serilizer = StudentSerilizer(stu)
+    # print('serilizer')
+    # print(serilizer)
+    # print('Serilizer data')
+    # print(serilizer.data)
+    # json_data = JSONRenderer().render(serilizer.data)
+    # print('Json data')
+    # print(json_data)
+    # return HttpResponse(json_data, content_type = 'application/json')
+    return JsonResponse(serilizer.data)
+
+
+
+def student_list(request):
+    stu = Student.objects.all()    
+    serilizer = StudentSerilizer(stu, many= True)
+    json_data = JSONRenderer().render(serilizer.data)
+    
+    # return HttpResponse(json_data, content_type = 'application/json')
+    return JsonResponse(serilizer.data, safe=False)
